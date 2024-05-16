05fea3e2760766f8fa975b00cf9b93c93e4ff9f2
Aki <mertunctuncer@gmail.com>
2024-05-17 01:11:04 +0300

    Added scope and pairing

--- app/src/main/java/com/github/mertunctuncer/projectsonar/model/bluetooth/AndroidBluetoothController.kt
+++ app/src/main/java/com/github/mertunctuncer/projectsonar/model/bluetooth/AndroidBluetoothController.kt
@@ -6,20 +6,22 @@ import com.github.mertunctuncer.projectsonar.domain.BluetoothDevice
 import com.github.mertunctuncer.projectsonar.model.bluetooth.service.AndroidBluetoothConnectionService
 import com.github.mertunctuncer.projectsonar.model.bluetooth.service.AndroidBluetoothScanService
 import com.github.mertunctuncer.projectsonar.domain.BluetoothConnection
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 
 class AndroidBluetoothController(
     override val context: Context,
-    bluetoothAdapter: BluetoothAdapter
+    bluetoothAdapter: BluetoothAdapter,
+    lifecycleScope: CoroutineScope
 ) : BluetoothController {
 
     private val bluetoothScanService = AndroidBluetoothScanService(context, bluetoothAdapter)
-    private val bluetoothConnectionService = AndroidBluetoothConnectionService(context, bluetoothAdapter, bluetoothScanService)
+    private val bluetoothConnectionService = AndroidBluetoothConnectionService(context, lifecycleScope, bluetoothAdapter, bluetoothScanService)
 
-    override val activeConnection: StateFlow<BluetoothConnection?> = bluetoothConnectionService.connection
+    override val activeConnection: StateFlow<BluetoothConnection?> get() =  bluetoothConnectionService.connection
 
-    override val scannedDevices: StateFlow<List<BluetoothDevice>> = bluetoothScanService.scannedDevices
-    override val pairedDevices: StateFlow<List<BluetoothDevice>> = bluetoothScanService.pairedDevices
+    override val scannedDevices: StateFlow<List<BluetoothDevice>> get() = bluetoothScanService.scannedDevices
+    override val pairedDevices: StateFlow<List<BluetoothDevice>> get() = bluetoothScanService.pairedDevices
 
     override fun startDiscovery() = bluetoothScanService.startDiscovery()
     override fun stopDiscovery() = bluetoothScanService.stopDiscovery()
@@ -30,9 +32,12 @@ class AndroidBluetoothController(
         bluetoothScanService.close()
     }
 
-    override val isConnected: StateFlow<Boolean> = bluetoothConnectionService.isConnected
+    override val isConnected: StateFlow<Boolean> get() = bluetoothConnectionService.isConnected
+    override val isScanning: StateFlow<Boolean> get() = bluetoothScanService.isScanning
 
     override suspend fun connect(device: BluetoothDevice): BluetoothConnection? = bluetoothConnectionService.connect(device)
+    override suspend fun pairAndConnect(device: BluetoothDevice) = bluetoothConnectionService.pairAndConnect(device)
+
     override fun disconnect() = bluetoothConnectionService.closeConnection()
 
 
