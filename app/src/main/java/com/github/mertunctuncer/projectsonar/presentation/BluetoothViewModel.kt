a6edebb2f1fa3817530fdc0023918eb83339aa09
Aki <mertunctuncer@gmail.com>
2024-05-17 01:12:13 +0300

    Added scan and pairing

--- app/src/main/java/com/github/mertunctuncer/projectsonar/presentation/BluetoothViewModel.kt
+++ app/src/main/java/com/github/mertunctuncer/projectsonar/presentation/BluetoothViewModel.kt
@@ -1,11 +1,14 @@
 package com.github.mertunctuncer.projectsonar.presentation
 
+import android.util.Log
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.mertunctuncer.projectsonar.domain.BluetoothDeviceData
 import com.github.mertunctuncer.projectsonar.model.bluetooth.BluetoothController
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -18,47 +21,59 @@ class BluetoothViewModel(
         val scannedDevices: List<BluetoothDeviceData> = emptyList(),
         val pairedDevices: List<BluetoothDeviceData> = emptyList(),
         val isConnected: Boolean = false,
+        val isScanning: Boolean = false,
         val isConnecting: Boolean = false,
         val errorMessage: String? = null,
     )
-    /*
-
-
 
     private val _state = MutableStateFlow(BluetoothUIState())
 
     val state = combine(
         bluetoothController.scannedDevices,
         bluetoothController.pairedDevices,
+        bluetoothController.isConnected,
+        bluetoothController.isScanning,
         _state
-    ) { scannedDevices, pairedDevices, state ->
+    ) { scannedDevices, pairedDevices, isConnected, isScanning, state ->
         state.copy(
             scannedDevices = scannedDevices,
-            pairedDevices = pairedDevices
+            pairedDevices = pairedDevices,
+            isConnected = isConnected,
+            isScanning = isScanning
         )
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _state.value)
 
     fun connect(device: BluetoothDeviceData) = viewModelScope.launch {
         _state.update { it.copy(isConnecting = true) }
-        val result = bluetoothController.connect(device).await()
-        if (result.isFailure) _state.update {
-            it.copy(
-                errorMessage = "Failed to connect to ${device.name}!"
-            )
-        } else {
-            _state.update {
+
+        bluetoothController.pairedDevices.collectLatest {pairedDevices ->
+            if(!pairedDevices.contains(device)) {
+                bluetoothController.pairAndConnect(device)
+                return@collectLatest
+            }
+
+            val result = bluetoothController.connect(device)
+            if (result == null) _state.update {
                 it.copy(
-                    isConnected = true,
-                    isConnecting = false,
-                    errorMessage = null
+                    errorMessage = "Failed to connect to ${device.name}!"
                 )
+            } else {
+                _state.update {
+                    it.copy(
+                        isConnected = true,
+                        isConnecting = false,
+                        errorMessage = null
+                    )
+                }
             }
         }
+
+
     }
 
 
     fun disconnect() {
-        bluetoothController.closeConnection()
+        bluetoothController.disconnect()
         _state.update { it.copy(
             isConnecting = false,
             isConnected = false
@@ -68,10 +83,16 @@ class BluetoothViewModel(
 
 
     fun startScan() {
+        _state.update {
+            it.copy(isScanning = true)
+        }
         bluetoothController.startDiscovery()
     }
 
     fun stopScan() {
+        _state.update {
+            it.copy(isScanning = false)
+        }
         bluetoothController.stopDiscovery()
     }
 
@@ -80,6 +101,4 @@ class BluetoothViewModel(
         bluetoothController.close()
     }
 
-
-     */
 }
